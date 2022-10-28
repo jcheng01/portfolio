79e9559d90584c86a947992f037c086b542c6140
jcheng01 <83311317+jcheng01@users.noreply.github.com>
2022-10-28 01:40:18 -0400

    update

--- justincheng/src/scripts/index.js
+++ justincheng/src/scripts/index.js
@@ -1,21 +1,9 @@
-
-
-
 import initTiltEffect from "vanilla-tilt/dist/vanilla-tilt";
 import AOS from 'aos';
 AOS.init();
 
 
-const hamburgerItem = document.querySelector('.hamburger-inner');
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('is-active');
-}, false);
 
 
 
-const bounce = document.querySelector('.buttonTwo');
-function animate () {
-  bounce.classList.add('animate__animated', 'animate__bounceOutLeft');
-}
 
--- justincheng/src/scripts/test.js
+++ justincheng/src/scripts/test.js
@@ -1,61 +1,78 @@
 function scrollAbout() {
-    let e = document.getElementById("aboutSection");
-    // This ends the block to the window 
-    // bottom and also aligns the view to the center 
-    e.scrollIntoView({
-      block: 'end',
-      behavior: 'smooth',
-      inline: 'center'
-    });
+  let e = document.getElementById("aboutSection");
+  // This ends the block to the window 
+  // bottom and also aligns the view to the center 
+  e.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+    inline: 'center'
+  });
 }
 function scrollExperience() {
-    let e = document.getElementById("experienceSection");
-    // This ends the block to the window 
-    // bottom and also aligns the view to the center 
-    e.scrollIntoView({
-      block: 'end',
-      behavior: 'smooth',
-      inline: 'nearest'
-    });
+  let e = document.getElementById("experienceSection");
+  // This ends the block to the window 
+  // bottom and also aligns the view to the center 
+  e.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+    inline: 'nearest'
+  });
 }
 function scrollProjects() {
-    let e = document.getElementById("projectsSection");
-    // This ends the block to the window 
-    // bottom and also aligns the view to the center 
-    e.scrollIntoView({
-      block: 'end',
-      behavior: 'smooth',
-      inline: 'center'
-    });
+  let e = document.getElementById("projectsSection");
+  // This ends the block to the window 
+  // bottom and also aligns the view to the center 
+  e.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+    inline: 'center'
+  });
 }
 function scrollContact() {
-    let e = document.getElementById("endSection");
-    // This ends the block to the window 
-    // bottom and also aligns the view to the center 
-    e.scrollIntoView({
-      block: 'end',
-      behavior: 'smooth',
-      inline: 'center'
-    });
+  let e = document.getElementById("endSection");
+  // This ends the block to the window 
+  // bottom and also aligns the view to the center 
+  e.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+    inline: 'center'
+  });
 }
 
 function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+const hamburgerItem = document.querySelectorAll('.hamburgerItem');
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobileNav');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  mobileNav.classList.toggle('overlay');
+  hamburgerItem.forEach(hamburgerItem => {
+    hamburgerItem.classList.toggle('is-active');
+  });
+}, false);
+
+hamburgerItem.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+
+}, false);
